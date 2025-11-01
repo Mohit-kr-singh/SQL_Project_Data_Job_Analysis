3d7c0fcb4f9c8330375cc7a84cee9283f9fb4d57
Mohit kumar singh <mohitmnq568@gmail.com>
2025-11-01 20:50:21 +0530

    Update 3_Top_demanded_Skills.sql

--- Project_sql/3_Top_demanded_Skills.sql
+++ Project_sql/3_Top_demanded_Skills.sql
@@ -1,7 +1,7 @@
 /*
 Question → What are the most in-demand skills for data analysts?
 Approach → Use an inner join between job_postings_fact and the relevant skills table (similar to Query 2) to analyze all job postings.
-Goal → Identify the top 5 most frequently requested skills for data analyst roles.
+Goal → Identify the top 10 most frequently requested skills for data analyst roles.
 Purpose → Understanding which skills are most in demand helps prioritize learning, align career development with market needs, and improve job readiness for high-impact roles.
 */
 
@@ -15,4 +15,4 @@ inner join skills_dim on skills_job_dim.skill_id=skills_dim.skill_id
 where job_title_short='Data Analyst' and job_location='India'
 GROUP BY skills
 ORDER BY demand_count desc
-limit 5
+limit 10
